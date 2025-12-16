6bc91b425c271f5a242ce9a89a5fcb74c9735f81
ZUBAIN NADEEM <zubainali23@gmail.com>
2025-12-17 02:12:59 +0500

    Age Checker Compeleted

--- AgeChecker/server.js
+++ AgeChecker/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const authRouter = require("./router/authRouter")
 
 app.use(express.json())
+
+app.use("/auth" , authRouter)
 
 app.listen(PORT, () =>
   console.log(`Server is running successfully on port ${PORT}`)
