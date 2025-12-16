bb88f6aad115ee2d95a11d5e6f5f0a3da659e29d
ZUBAIN NADEEM <zubainali23@gmail.com>
2025-12-17 02:06:40 +0500

    middle ware and controller set

---
+++ AgeChecker/middleware/ageChecker.js
@@ -0,0 +1,15 @@
+const ageChecker = (req, res, next) => {
+
+  const age = req.body.age;
+
+  if(age === undefined){
+    return res.send(`Age is not Define`);
+  }
+  if (age < 18) {
+    return res.send(`you are not eligible`);
+  }
+
+  next();
+};
+
+module.exports = ageChecker;
